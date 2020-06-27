ef1ed8dad407036e413891f84b7a85257dd6e847
emy-sun <ira.lemeshevskaja@yandex.by>
2020-06-27 22:32:10 +0300

    fix bug style

--- portfolio/src/app/shared/components/table/table.module.ts
+++ portfolio/src/app/shared/components/table/table.module.ts
@@ -5,12 +5,17 @@ import { MatTableModule } from '@angular/material/table';
 import { MatSortModule } from '@angular/material/sort';
 import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatVideoModule } from 'mat-video';
 
 @NgModule({
-    declarations: [ TableComponent ],
-    imports: [ CommonModule, MatTableModule, MatSortModule, MatPaginatorModule, MatIconModule, MatVideoModule ],
-    exports: [ TableComponent ],
-    providers: [ DatePipe ]
+  declarations: [TableComponent],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatIconModule,
+  ],
+  exports: [TableComponent],
+  providers: [DatePipe],
 })
 export class TableModule {}
